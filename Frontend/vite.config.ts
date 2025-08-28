bb53d8612e623a89d63f0e12d8fb683d275a67c2
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-08-28 19:22:10 +0700

    fix: vite config merge

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,22 +1,23 @@
 /// <reference types="vite/client" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import path from "path";
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
+
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["manifest.json"],
+      registerType: 'autoUpdate',
+      includeAssets: ['manifest.json'],
       workbox: {
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com/,
-
-            handler: "CacheFirst",
+            handler: 'CacheFirst',
             options: {
-              cacheName: "google-fonts",
+              cacheName: 'google-fonts',
               expiration: {
                 maxEntries: 10,
                 // 30 days in seconds
@@ -28,48 +29,58 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
       },
       manifest: {
-        name: "Zenium",
-        short_name: "Zenium",
-        description: "Zenium: AI with empathy, IoT with soul.",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-        orientation: "portrait-primary",
+        name: 'Zenium',
+        short_name: 'Zenium',
+        description: 'Zenium: AI with empathy, IoT with soul.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        orientation: 'portrait-primary',
         icons: [
           {
-            src: "/PWA/logo.png",
-            type: "image/png",
-            sizes: "192x192",
-            purpose: "any",
+            src: '/PWA/logo.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'any',
           },
           {
-            src: "/PWA/logo.png",
-            type: "image/png",
-            sizes: "512x512",
-            purpose: "any",
+            src: '/PWA/logo.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any',
           },
         ],
         screenshots: [
           {
-            src: "/PWA/logo.png",
-            sizes: "1280x720",
-            type: "image/png",
-            form_factor: "wide",
+            src: '/PWA/logo.png',
+            sizes: '1280x720',
+            type: 'image/png',
+            form_factor: 'wide',
           },
           {
-            src: "/PWA/logo.png",
-            sizes: "375x667",
-            type: "image/png",
+            src: '/PWA/logo.png',
+            sizes: '375x667',
+            type: 'image/png',
           },
         ],
-        lang: "en",
+        lang: 'en',
       },
     }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': process.env,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
     },
   },
 });
