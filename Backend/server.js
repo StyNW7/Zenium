179c069b9392ac41fb6a6d0844c23d287f194651
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-08-28 19:01:34 +0700

    deploy: be on vercel

--- Backend/server.js
+++ Backend/server.js
@@ -9,24 +9,60 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173"], // fe origin
-    credentials: true,
-  })
-);
+// CORS configuration
 
+let corsOptions;
+
+if (process.env.NODE_ENV === "development") {
+  corsOptions = {
+    origin: (origin, callback) => {
+      // Jika origin tidak ada (contoh: Postman, curl), izinkan
+      if (!origin) return callback(null, true);
+      return callback(null, true); // Semua origin diizinkan
+    },
+    credentials: true,
+  };
+} else {
+  corsOptions = {
+    credentials: true,
+  };
+  corsOptions.origin = "http://zenium-frontend.vercel.app/";
+}
+
+app.use(cors(corsOptions));
+
+// Routes
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/api", router);
 
-connectDB()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
+// For Vercel deployment
+export default async (req, res) => {
+  try {
+    await connectDB();
+    return app(req, res);
+  } catch (err) {
     console.error("Failed to connect to DB", err);
-    process.exit(1);
-  });
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// For local development
+if (process.env.NODE_ENV === "development") {
+  const startServer = async () => {
+    try {
+      await connectDB();
+      app.listen(port, () => {
+        console.log(`Server running in development mode`);
+        console.log(`Server running on port ${port}`);
+        console.log(`Access the server at http://localhost:${port}`);
+      });
+    } catch (err) {
+      console.error("Failed to connect to DB", err);
+      process.exit(1);
+    }
+  };
+  startServer();
+}
