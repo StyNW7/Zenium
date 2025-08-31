c19f9429dda31e6ab37c016f5175e1edca34f855
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-08-31 17:08:23 +0700

    fix deployment

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,18 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({
-  onNeedRefresh() {
-    // you can show a toast/banner to let user refresh
-    console.log('New content available, please refresh.')
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline.')
-  },
-})
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
